4393bc157ac8330154c9fb34c872ba64dc9ea1d1
Rupraj Singh <ruprajsingh1@gmail.com>
2025-04-13 18:40:23 +0530

    done now

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import Banner from "./Banner";
-import Category from "./AllProducts";
 import Showcase from "./Showcase";
 import { Separator } from "@/components/ui/separator";
 import Featured from "./Featured";
-import InstagramPage from "./Insta";
 import AllProducts from "./AllProducts";
 
 const page = () => {
@@ -12,12 +10,11 @@ const page = () => {
     <div className="overflow-x-hidden">
       <Banner />
       <Showcase />
-      
+
       <Separator />
       <Featured />
       <Separator />
-      <AllProducts/>
-   
+      <AllProducts />
     </div>
   );
 };
